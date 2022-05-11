112e850c06e49d2c9c4ec81de17a971e476b0c3f
JESUS ANTONIO CASTILLO PANTOJA <101521260+antoniocastillopantoja@users.noreply.github.com>
2022-05-11 15:48:25 -0600

    subo reto 2

--- Reto02/reto003.js
+++ Reto02/reto003.js
@@ -1,11 +1,10 @@
-let arregloImpar=[1,2,3,4,5,6,7,8,9,10];
+let arreglo=[1,2,3,4,5,6,7,8,9,10];
 
-arregloImpar.forEach(function(numero){
-    if(numero % 2===0){
-        console.log('el numero ' + numero +  ' el numero es par ')
-    }else{
-        console.log('el numero ' +numero+  ' el numero es Impar ')
+arreglo.forEach((elemento)=>{
+    if (elemento % 2 === 0) {
+        console.log('el numero ' + elemento +  ' el numero es par ');      
+    }
+    else {
+        console.log('el numero ' + elemento +  ' el numero es impar ');
     }
 })
-
-
