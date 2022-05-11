6127568266447190bacc748a911f3d030395c52c
JESUS ANTONIO CASTILLO PANTOJA <101521260+antoniocastillopantoja@users.noreply.github.com>
2022-05-11 15:54:11 -0600

    subo reto de las tabla de multiplicar

--- Reto03/reto004.js
+++ Reto03/reto004.js
@@ -2,6 +2,6 @@
 let tabla=5;
 let cantidad= 12;
 
-for(let i=1 ; i <=cantidad ;i++){
-    console.log( tabla + 'x' + i + '='+(tabla*i));
+for(let i=1; i<=cantidad;i++){
+    console.log(`${tabla} x ${i} = ${tabla*i}`);
 }
